e188f48a831fcfc9c8941edfc74076f5ad7d6177
artS3rg <art.serg.3002@mail.ru>
2024-10-09 20:49:29 +0300

    new

---
+++ app/src/main/java/com/artinc/planit/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.artinc.planit
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val createBtn = findViewById<Button>(R.id.createTask)
+        createBtn.setOnClickListener {
+            Toast
+                .makeText(this@MainActivity, "Создание новой задачи", Toast.LENGTH_LONG)
+                .show()
+        }
+    }
+}
